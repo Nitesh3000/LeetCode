12169863892bdaaac96564e2a6a6c02906d415eb
Nitesh Sharma <70530546+Nitesh3000@users.noreply.github.com>
2025-01-29 13:25:44 +0530

    Time: 0 ms (100%), Space: 22.9 MB (57.91%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,56 +11,34 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
+        if(head == nullptr || head->next == nullptr){
             return;
         }
-        ListNode* middle = findMiddle(head);
-        ListNode* head2 = middle->next;
-        middle->next = nullptr;
-        head2 = reverseList(head2);
-        ListNode* temp = head2;
-        while(temp!=nullptr){
-            cout<<temp->val<<endl;
-            temp = temp->next;
-        }
-        combine(head,head2);
-        
-    }
-    ListNode* findMiddle(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast->next !=nullptr && fast->next->next != nullptr){
+         //1. Find Middle
+         ListNode* fast = head;
+         ListNode* slow = head;
+         while(fast->next!=nullptr && fast->next->next!=nullptr){
             slow = slow->next;
             fast = fast->next->next;
-        }
-        return slow;
-    }
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prevNode = nullptr;
-        ListNode* currentNode = head;
-        while (currentNode != nullptr) {
-            ListNode* nextNode = currentNode->next;
-            currentNode->next = prevNode;
-            prevNode = currentNode;
-            currentNode = nextNode;
-        }
-        return prevNode;  
-    }
-    void combine(ListNode* head, ListNode* head2) {
+         }
+
+         //2. Rerverse the second half
+         ListNode* first = slow;
+         ListNode* second = slow->next;
+         while(second!=nullptr){
+            ListNode* third = second->next;
+            second->next = first;
+            first = second;
+            second = third;
+         }
+         slow->next = nullptr;
         ListNode* head1 = head;
-        while (head2 != nullptr) {
-           
-            ListNode* temp1 = head1->next;
-            ListNode* temp2 = head2->next;
-
-           
-            head1->next = head2;
-            head2->next = temp1;
-
-          
-            head1 = temp1;
-            head2 = temp2;
+         //3. Merge the Two halves
+        while(head1!=nullptr){
+            ListNode* temp = head1->next;
+            head1->next = first;
+            head1 = first;
+            first = temp;
         }
     }
-
 };
